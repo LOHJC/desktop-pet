18b8bc1e7f9111b2d00d295ed51d4660546d3356
lohjc <jiachiewloh@gmail.com>
2026-04-03 23:24:38 +0800

    init window

--- main.py
+++ main.py
@@ -1,3 +1,57 @@
 
+
+from PyQt5 import QtGui, QtCore, QtWidgets
+import sys
+
+class PetHouse(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # borderless, transparent and stay on top
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.Tool)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        # this is for calculate dragging
+        self.mouseDragPos = 0
+    
+    def paintEvent(self, a0):
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+
+        # Fill entire window with a color
+        painter.setBrush(QtGui.QColor(100, 180, 250, 255))  # RGBA
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.drawRect(self.rect())
+
+        return super().paintEvent(a0)
+    
+    def mousePressEvent(self, a0):
+        # TOFIX: temp using rightclick to close the window
+        if (a0.button() == QtCore.Qt.RightButton):
+            self.close()
+            QtWidgets.QApplication.instance().quit()
+        self.mouseDragPos = a0.globalPos()
+        return super().mousePressEvent(a0)
+    
+    def mouseMoveEvent(self, a0):
+        delta = QtCore.QPoint(a0.globalPos() - self.mouseDragPos)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.mouseDragPos = a0.globalPos()
+
+        return super().mouseMoveEvent(a0)
+
+
+class PetLoader():
+    def __init__(self, petname):
+        self.idle_sprites = []
+        self.walking_sprites = []
+
 if __name__ == "__main__":
+    # create a borderless window
+    app = QtWidgets.QApplication([])
+    window = PetHouse()
+    window.show()
+
+    # load the sprites
     pass
+
+    sys.exit(app.exec_())
--- readme.md
+++ readme.md
@@ -1,11 +1,32 @@
 # desktop-pet
 a desktop companion
 
+## mode
+- [ ] small window mode
+  - the pet is contains in the small environment
+- [ ] taskbar mode
+  - the pet should run around the taskbar
+
 ## todo
 - [x] find asset resources
   - create using piskelapp
 - [ ] show animation
-  - [ ] generate a transparent window
-  - [ ] init some settings (eg. drag, minimize etc)
+  - [ ] animate the pet in the window
+- [x] generate a transparent window
+- [x] init some settings
+  - [x] drag
+    - click and drag around the window area
+  - [x] close
+    - temporary right click on the window to close it
 - [ ] read keystrokes to have something like powermode/charging
 - [ ] able to set timer for reminder
+
+## setup
+- install python
+- `pip install uv`
+- within this folder, `uv venv`
+- activate the venv by `.venv/Scripts/activate`
+- install dependencies by `uv pip install XXX`
+
+### dependencies
+- pyqt5
